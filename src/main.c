92ea29d89a2f97626509a6560e9aa33725c9108c
Marcos Montiel <marcosmontielrej@gmail.com>
2025-07-12 20:46:20 -0400

    Adding template to main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+
 int main(void) {
+  printf("Let's the Battle Begin!\n\n");
+
+  int exit = 0;
+  int first_time = 1;
+
+  while (exit == 0) {
+    if (first_time) {
+      //TODO: make the selection of the 3 initial pokemons
+    }
+    //TODO: Start the battles
+
+    //TODO: Ask for input
+    char* input = "";
+
+    //TODO: Handle Input
+    if (input == "q" || input == "Q") {
+      break;
+    }
+    
+  }
+
+  printf("Thank you for playing!\n");
+  
   return 0;
 }
